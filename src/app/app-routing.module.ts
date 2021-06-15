2ba907a1d51df88086f313942e6aca3e769b2652
Ahmed Mahmoud <ahmedmahmoudabdelaty@gmail.com>
2021-06-15 23:28:11 +0200

    added new controller

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppDetailsComponent } from './components/app-details/app-details.component';
 import { AppItemComponent } from './components/app-item/app-item.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'home/:id', component: HomeComponent }
+  { path: 'home/:id', component: HomeComponent },
+  { path: 'appDetails/:id', component: AppDetailsComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,24 @@ import { AppComponent } from './app.component';
 import { AppNavbarComponent } from './components/navbar/app-navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AppItemComponent } from './components/app-item/app-item.component';
+import { AppDetailsComponent } from './components/app-details/app-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppNavbarComponent,
     HomeComponent,
-    AppItemComponent
+    AppItemComponent,
+    AppDetailsComponent
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    
+
   ],
   providers: [],
   bootstrap: [AppComponent],
-  
+
 })
 export class AppModule { }
